82041f3edac685eaf076a34d3e50b3351fc85f5e
ANDRI ANDRI <47277287+lamcodeofpwnosec@users.noreply.github.com>
2024-11-07 23:34:24 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ import { AWSPollyEngine } from "./types/aws-polly-engine";
 
 // Environment variables
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); 
 
 // Config Interface
 interface IConfig {
